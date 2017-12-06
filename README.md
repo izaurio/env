a74771abedde5b9ecff72c56ab5038081bb53adb
Zaur Abasmirzoev <i@zaur.io>
2017-12-06 22:46:21 +0300

    Removed unused settings

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 ## Git
 
 ```
-git config --global core.editor /usr/bin/vim
+git config --global user.name "Zaur Abasmirzoev"
+git config --global user.email i@zaur.io
+git config --global core.editor vim
 ```
 
--- common.sh
+++ common.sh
@@ -1,3 +1,4 @@
+# startup
 umask 002
 echo
 touch ~/.hushlogin
@@ -9,41 +10,52 @@ echo
 # homebrew
 export PATH=~/bin:/usr/local/bin:/usr/local/sbin:$PATH
 
+# go
+export GOPATH=/Users/zaur/go
+export PATH=$PATH:$GOPATH/bin
+
+# nodejs
+export PATH=/usr/local/share/npm/bin:$PATH
+export PATH=/usr/local/lib/node_modules:$PATH
+# old node
+export PATH="/usr/local/opt/node@6/bin:$PATH"
+
+# perlbrew
+source ~/perl5/perlbrew/etc/bashrc
+
+# plenv
+if which plenv > /dev/null; then eval "$(plenv init - zsh)"; fi
+export PATH="$HOME/.plenv/bin:$PATH"
 
 # rbenv
 # To use Homebrew's directories rather than ~/.rbenv add to your profile:
 # export RBENV_ROOT=/usr/local/var/rbenv
 #To enable shims and autocompletion add to your profile:
-#if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 # path for rbenv
-#export PATH=~/.rbenv/bin:$PATH
+export PATH=~/.rbenv/bin:$PATH
 # brew install curl-ca-bundle
 #export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
 
-# go
-export GOPATH=/Users/zaur/golib
-export PATH=$PATH:$GOPATH/bin
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+export BUNDLE_GITHUB__COM=FOOBAR:x-oauth-basic
 
+# java
+export JAVA_HOME=`/usr/libexec/java_home -v 1.9`
+export PATH=${JAVA_HOME}/bin:$PATH
 
-# nodejs
-export PATH=/usr/local/share/npm/bin:$PATH
-export PATH=/usr/local/lib/node_modules:$PATH
-
-# perlbrew
-# source ~/perl5/perlbrew/etc/bashrc
-
+# locale
 export LANG="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
+# theme
+export TERM=xterm-256color
 
-alias mongo.start="mongod run --config /usr/local/etc/mongod.conf"
+# aliases
 alias redis.start="redis-server /usr/local/etc/redis.conf"
 alias redis.stop="redis-cli shutdown"
-alias recreate="rake db:drop && rake db:create && rake db:migrate && rake db:test:prepare"
 alias pg.start="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
 alias pg.stop="pg_ctl -D /usr/local/var/postgres stop -s -m fast"
 alias flushdns="sudo discoveryutil udnsflushcaches"
-
-#export TERM=xterm-256color
-
